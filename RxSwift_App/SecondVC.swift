b8055f83e5085c775ee10c233d676117f991be33
Eyad Heikal <eyadheikal@gmail.com>
2020-05-01 15:42:49 +0200

    3rd Commit

---
+++ RxSwift_App/SecondVC.swift
@@ -0,0 +1,30 @@
+//
+//  SecondVC.swift
+//  RxSwift_App
+//
+//  Created by Eyad Heikal on 5/1/20.
+//  Copyright © 2020 Eyad Heikal. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
